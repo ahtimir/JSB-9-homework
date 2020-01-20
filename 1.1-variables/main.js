467a96cab1b200917962c06399e37e58deffb73b
ahtimir <ahtimir@gmail.com>
2020-01-20 12:52:48 +0300

    Modified main.js

---
+++ 1.1-variables/main.js
@@ -0,0 +1,23 @@
+//Задачи 1 и 2
+
+let myName = "Мария Степановна";
+console.log(`Привет, мир! Меня зовут ${myName}`);
+
+
+let algebra = 3;
+let geography = 5;
+let physics = 4;
+let medianScore;
+
+medianScore = (algebra + geography + physics) / 3;
+console.log(`Средняя оценка: ${medianScore}`);
+
+//Задача 3
+
+let x = 2;
+let y = 22;
+let z = 0;
+
+result = x * y + 5 * z + x - 1;
+console.log(`Результат выражения: ${result}`);
+
